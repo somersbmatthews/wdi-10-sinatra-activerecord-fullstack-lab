3fd3a5a79980a2ac3d965610a30fc48c20ff661c
Somers Matthews <somersbmatthews@uchicago.edu>
2018-02-05 01:16:22 -0600

    created hello.rb

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -3,6 +3,8 @@ class ApplicationController < Sinatra::Base
 		require 'bundler'
 		Bundler.require()
 
+		set :views, File.expand_path('../views', File.dirname(__FILE__))
+
 		get '/' do
 			"The server is armed and operational."
 		end
---
+++ views/hello.rb
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+	<h1>Awesome Item App (this h1 is in hello.erb)</h1>
+</body>
+</html>
